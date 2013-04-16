83fadf574698e1188edfb19fc255a533486cb53b
Sigtryggur <sigtrygg@mit.edu>
2013-04-16 03:14:24 -0400

    unit tests updated

--- test/unit/sticky_test.rb
+++ test/unit/sticky_test.rb
@@ -14,30 +14,35 @@ require 'test_helper'
 #	4.Empty update
 
 class StickyTest < ActiveSupport::TestCase
+    def repr(sticky)
+      dict={:text=>sticky.text,:position=>YAML::load(sticky.position),:textProperties=>YAML::load(sticky.textProperties)}
+    return dict.to_json.to_s
+  end
+
+
   test "merge temp and stored" do
     sticky1=stickies(:one)
     sticky2=stickies(:two)
-    temporary=Marshal::dump({"new23"=>"ihtfp","new42"=>"jon"})
-    expected={sticky1.id.to_s=>sticky1.text,sticky2.id.to_s=>sticky2.text,"new23"=>"ihtfp","new42"=>"jon"}
+    sticky3={:text=>"ihtfp",:position=>{:top=>0,:left=>0},:textProperties=>{:'font-weight'=>"bold",:'font-style'=>"italic",:'text-decoration'=>"underline"}}
+    temporary=Marshal::dump({"new23"=>sticky3.to_json.to_s})
+    expected={sticky1.id.to_s=>repr(sticky1),sticky2.id.to_s=>repr(sticky2),"new23"=>sticky3.to_json.to_s}
     got=Marshal::load(Sticky.merge_temporary_with_stored(temporary,1))
     assert_equal expected,got
   end 
-  
   test "merge empty temp and stored" do
     sticky1=stickies(:one)
     sticky2=stickies(:two)
     temporary=Marshal::dump({})
-    expected={sticky1.id.to_s=>sticky1.text,sticky2.id.to_s=>sticky2.text}
+    expected={sticky1.id.to_s=>repr(sticky1),sticky2.id.to_s=>repr(sticky2)}
     got=Marshal::load(Sticky.merge_temporary_with_stored(temporary,1))
     assert_equal expected,got
-  end
-  
+  end 
   test "merge temp and empty stored" do
-    expected={"new23"=>"ihtfp","new42"=>"jon"}
+    sticky1={:text=>"ihtfp",:position=>{:top=>0,:left=>0},:textProperties=>{:'font-weight'=>"bold",:'font-style'=>"italic",:'text-decoration'=>"underline"}}.to_json.to_s
+    expected={"new1"=>sticky1}
     temporary=Marshal::dump(expected)
     got=Marshal::load(Sticky.merge_temporary_with_stored(temporary,2))
     assert_equal expected,got
-
   end
   test "empty merge" do 
     temporary=Marshal::dump({})
@@ -48,68 +53,60 @@ class StickyTest < ActiveSupport::TestCase
   end
   test "only updates" do
     sticky1=stickies(:one)
-    sticky2=stickies(:two)
+    text="hello"
+    position={:top=>0,:left=>0}.to_yaml
+    textProperties={:'font-weight'=>400,:'font-style'=>"normal",:'text-decoration'=>"none"}.to_yaml
     assert_equal "hello",sticky1.text
-    assert_equal "world",sticky2.text
+    assert_equal position,sticky1.position
+    assert_equal textProperties,sticky1.textProperties
    
-    text1="you are the weakest link!"
-    text2="goodbye"
-    
-    stickies={sticky1.id.to_s=>text1,sticky2.id.to_s=>text2}
+    text1="you are the weakest link! goodbye"
+    position1={"top"=>100,"left"=>100}
+    textProperties1={"font-weight"=>700,'font-style'=>"italic",'text-decoration'=>"underline"}
+    dict={:text=>text1,:position=>position1,:textProperties=>textProperties1}.to_json.to_s    
+    stickies={sticky1.id.to_s=>dict}
    
     Sticky.update_or_create_stickies(stickies,1)
    
     sticky1=Sticky.find(sticky1.id)
-    sticky2=Sticky.find(sticky2.id)
     assert_equal text1,sticky1.text
-    assert_equal text2,sticky2.text
+    assert_equal position1,YAML::load(sticky1.position)
+    assert_equal textProperties1,YAML::load(sticky1.textProperties)
   end
-
   test "only create" do
-    stickies={"new23"=>"ihtfp","new42"=>"jon"}
+    sticky1={:text=>"ihtfp",:position=>{:top=>0,:left=>0},:textProperties=>{:'font-weight'=>"bold",:'font-style'=>"italic",:'text-decoration'=>"underline"}}.to_json.to_s
+    stickies={"new1"=>sticky1}
     Sticky.update_or_create_stickies(stickies,2)
     
-    newstickies=Sticky.find_all_by_user_id(2)
-    
-    assert_equal newstickies.length,2 
-    sticky1=newstickies[0]
-    sticky2=newstickies[1]
-    assert ((sticky1.text=="ihtfp" and sticky2.text="jon") or (sticky1.text=="jon" and sticky2.text="ihtfp"))
+    newsticky=Sticky.find_by_user_id(2)
+    assert newsticky.text=="ihtfp"
      
   end
-
   test "update and create" do
-    require 'set'
     sticky1=stickies(:one)
     sticky2=stickies(:two)
+ 
+    text1="you are the weakest link! goodbye"
+    position1={"top"=>100,"left"=>100}
+    textProperties1={"font-weight"=>700,'font-style'=>"italic",'text-decoration'=>"underline"}
+    dict={:text=>text1,:position=>position1,:textProperties=>textProperties1}.to_json.to_s
+
+    sticky2={:text=>"ihtfp",:position=>{:top=>0,:left=>0},:textProperties=>{:'font-weight'=>"bold",:'font-style'=>"italic",:'text-decoration'=>"underline"}}.to_json.to_s
     
-    text1="you are the weakest link!"
-    text2="goodbye"
-    text3="ihtfp"
-    text4="jon"
-
-    expected=Set.new
-    expected.merge([text1,text2,text3,text4])
-
-    stickies={sticky1.id.to_s=>text1,sticky2.id.to_s=>text2,"new23"=>text3,"new42"=>text4}
+    stickies={sticky1.id.to_s=>dict,"new1"=>sticky2}
     
     Sticky.update_or_create_stickies(stickies,1)
 
-    newstickies=Sticky.all.map{|sticky| sticky.text}
-    got=Set.new
-    got.merge(newstickies)
-
-    assert_equal 4, newstickies.length
-
-    assert_equal expected,got    
+    newstickies=Sticky.find_all_by_user_id(1)
+    
+    assert_equal 3,newstickies.length
+    
 
   end
-  
   test "empty update and empty create" do
     stickies={}
     Sticky.update_or_create_stickies(stickies,2)
     newstickies=Sticky.find_all_by_user_id(2)
     assert_equal 0, newstickies.length
   end
-
 end
