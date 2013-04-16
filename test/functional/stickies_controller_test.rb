2daf642853cb9705f4c7646d6a0c760997bb16cb
Sigtryggur <sigtrygg@mit.edu>
2013-04-16 03:31:10 -0400

    Functional tests updated and expanded

--- test/functional/stickies_controller_test.rb
+++ test/functional/stickies_controller_test.rb
@@ -7,21 +7,42 @@ require 'test_helper'
 #Delete-sticky => 'id' should be removed from cookies
 #
 class StickiesControllerTest < ActionController::TestCase
+  new='{text:"",position:{top:200,left:50},textProperties:{"font-weight":400,"font-style":"normal","text-decoration":"none"}'
 
   test "should add empty to cookies" do
     id="new123"
-    post :create_sticky,{:id=>id}
+    post :create_sticky,{:id=>id,:attr=>new}
     stickies=Marshal::load(cookies[:stickies])
-    assert_equal "",stickies[id]
+    assert_equal new,stickies[id]
   end
 
   test "should update text in cookie" do
     id="new123"
-    text="Hello Word"
-    post :create_sticky,{:id=>id}
-    post :update_sticky,{:id=>id,:text=>text}
+    post :create_sticky,{:id=>id,:attr=>new}
+    update='{text:"HELLO WORLD",position:{top:200,left:50},textProperties:{"font-weight":400,"font-style":"normal","text-deco
+ration":"none"}'
+    post :update_sticky,{:id=>id,:attr=>update}
     stickies=Marshal::load(cookies[:stickies])
-    assert_equal text,stickies[id]
+    assert_equal update,stickies[id]
+  end
+
+  test "should update position in cookie" do
+    id="new123"
+    post :create_sticky,{:id=>id,:attr=>new}
+    update='{text:"",position:{top:500,left:-10},textProperties:{"font-weight":400,"font-style":"normal","text-deco
+ration":"none"}'
+    post :update_sticky,{:id=>id,:attr=>update}
+    stickies=Marshal::load(cookies[:stickies])
+    assert_equal update,stickies[id]
+  end
+
+  test "should update textProperties in cookie" do
+    id="new123"
+    post :create_sticky,{:id=>id,:attr=>new}    
+    update='{text:"",position:{top:200,left:50},textProperties:{"font-weight":700,"font-style":"italic","text-decoration":"underline"}'
+    post :update_sticky,{:id=>id,:attr=>update}
+    stickies=Marshal::load(cookies[:stickies])
+    assert_equal update,stickies[id]
   end
 
   test "should remove sticky from cookies" do
